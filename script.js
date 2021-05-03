58b9f264a15e42046c16cdab651063aa9beb7d35
MostafaShelim <mostafarmstu@gmail.com>
2021-05-03 23:15:52 +0600

    inheritence with prototype based

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-// class is not understood by js it always works with old fashion but programmer want class
-
-class Person {
-    constructor(name, age){// prototype er baccha hey tu
-        this.name = name;
-        this.age = age;
-    }
-    eat(){// prototype er baccha hey tu
-        console.log('eating');
-    }
-    play(){// prototype er baccha hey tu
-        console.log('playing');
-    }
+// Inheritence with prototype based
+function Person(name, age){
+    this.name = name;
+    this.age = age;
 }
-const me = new Person('mostafa',23);
-me.play();
-console.dir(Person)
+Person.prototype.eat = function(){
+    console.log(`${this.name} is eating`);
+}
+
+function Cricketer(name, age, type, country){
+    Person.call(this, name, age);
+    // this.name = name;
+    this.type = type;
+    this.country = country;
+}
+
+Cricketer.prototype = Object.create(Person.prototype);
+Cricketer.prototype.constructor = Cricketer;
+
+const mushi = new Cricketer('mushi', 35, 'wk-batsman', 'bangladesh');
+mushi.eat();
+console.log(mushi.name);
