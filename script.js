dd7cfc0db528e5715ca4a3c3ea4ed338d8c89e2b
MostafaShelim <mostafarmstu@gmail.com>
2021-05-03 23:07:35 +0600

    constructor function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Factory function
+
+// consturctor function
 
 function createCircle(radius){
-    return{
-        radius,
-        draw: function(){
-            console.log('draw');
-        }
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
     };
+    //return this
 };
-
-const cicle = createCircle(1);
-cicle.draw();
+const circle = new createCircle(1);// new keyword needed
+circle.draw();
+console.log(circle.radius);
